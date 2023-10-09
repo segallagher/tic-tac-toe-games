800baddca10da27469cbaf93b771471c3de7145f
Samuel Gallagher <segallagher@alaska.edu>
2023-10-09 00:13:59 -0800

    added more tests

--- RulesTest.cpp
+++ RulesTest.cpp
@@ -8,11 +8,17 @@
 #include "Rules.h" // Double-inclusion test
 #include "doctest.h"
 
-TEST_CASE("Board Checks")
-{
-	
+TEST_CASE("Empty Board") {
+	std::vector<std::vector<TileType>> board{
+		{TileType::Empty,TileType::Empty,TileType::Empty},
+		{TileType::Empty,TileType::Empty,TileType::Empty},
+		{TileType::Empty,TileType::Empty,TileType::Empty}
+	};
 
-	// Test values
+	CHECK(score(GameMode::nRow, board) == false);
+}
+
+TEST_CASE("Board Horizontal") {
 	std::vector<std::vector<TileType>> board{
 		{TileType::X,TileType::X,TileType::X},
 		{TileType::Empty,TileType::Empty,TileType::Empty},
@@ -20,5 +26,130 @@ TEST_CASE("Board Checks")
 	};
 
 	CHECK(score(GameMode::nRow, board) == true);
+
+	board = {
+		{TileType::Empty,TileType::Empty,TileType::Empty},
+		{TileType::O,TileType::O,TileType::O},
+		{TileType::Empty,TileType::Empty,TileType::Empty}
+	};
+
+	CHECK(score(GameMode::nRow, board) == true);
+
+	board = {
+		{TileType::Empty,TileType::Empty,TileType::Empty},
+		{TileType::Empty,TileType::Empty,TileType::Empty},
+		{TileType::X,TileType::X,TileType::X}
+	};
+
+	CHECK(score(GameMode::nRow, board) == true);
+
+	board = {
+		{TileType::O,TileType::X,TileType::O},
+		{TileType::Empty,TileType::Empty,TileType::Empty},
+		{TileType::Empty,TileType::Empty,TileType::Empty}
+	};
+
+	CHECK(score(GameMode::nRow, board) == false);
+
+	board = {
+		{TileType::Empty,TileType::Empty,TileType::Empty},
+		{TileType::O,TileType::X,TileType::Empty},
+		{TileType::Empty,TileType::Empty,TileType::Empty}
+	};
+
+	CHECK(score(GameMode::nRow, board) == false);
+
+	board = {
+		{TileType::Empty,TileType::Empty,TileType::Empty},
+		{TileType::Empty,TileType::Empty,TileType::Empty},
+		{TileType::X,TileType::Empty,TileType::X}
+	};
+
+	CHECK(score(GameMode::nRow, board) == false);
+}
+
+TEST_CASE("Board Vertical") {
+	std::vector<std::vector<TileType>> board{
+		{TileType::X,TileType::Empty,TileType::Empty},
+		{TileType::X,TileType::Empty,TileType::Empty},
+		{TileType::X,TileType::Empty,TileType::Empty}
+	};
+
+	CHECK(score(GameMode::nRow, board) == true);
+
+	board = {
+		{TileType::Empty,TileType::O,TileType::Empty},
+		{TileType::Empty,TileType::O,TileType::Empty},
+		{TileType::Empty,TileType::O,TileType::Empty}
+	};
+
+	CHECK(score(GameMode::nRow, board) == true);
+
+	board = {
+		{TileType::Empty,TileType::Empty,TileType::X},
+		{TileType::Empty,TileType::Empty,TileType::X},
+		{TileType::Empty,TileType::Empty,TileType::X}
+	};
+
+	CHECK(score(GameMode::nRow, board) == true);
+
+	board = {
+		{TileType::X,TileType::Empty,TileType::Empty},
+		{TileType::X,TileType::Empty,TileType::Empty},
+		{TileType::O,TileType::Empty,TileType::Empty}
+	};
+
+	CHECK(score(GameMode::nRow, board) == false);
+
+	board = {
+		{TileType::Empty,TileType::O,TileType::Empty},
+		{TileType::Empty,TileType::O,TileType::Empty},
+		{TileType::Empty,TileType::X,TileType::Empty}
+	};
+
+	CHECK(score(GameMode::nRow, board) == false);
+
+	board = {
+		{TileType::Empty,TileType::Empty,TileType::X},
+		{TileType::Empty,TileType::Empty,TileType::O},
+		{TileType::Empty,TileType::Empty,TileType::X}
+	};
+
+	CHECK(score(GameMode::nRow, board) == false);
+}
+
+TEST_CASE("Board Diagonal")
+{
+	std::vector<std::vector<TileType>> board{
+		{TileType::X,TileType::Empty,TileType::Empty},
+		{TileType::Empty,TileType::X,TileType::Empty},
+		{TileType::Empty,TileType::Empty,TileType::X}
+	};
+
+	CHECK(score(GameMode::nRow, board) == true);
+
+	board = {
+		{TileType::Empty,TileType::Empty,TileType::O},
+		{TileType::Empty,TileType::O,TileType::Empty},
+		{TileType::O,TileType::Empty,TileType::Empty}
+	};
+
+	CHECK(score(GameMode::nRow, board) == true);
+
+	board = {
+		{TileType::X,TileType::Empty,TileType::O},
+		{TileType::Empty,TileType::X,TileType::Empty},
+		{TileType::O,TileType::Empty,TileType::Empty}
+	};
+
+	CHECK(score(GameMode::nRow, board) == false);
+
+	board = {
+		{TileType::Empty,TileType::Empty,TileType::O},
+		{TileType::Empty,TileType::X,TileType::Empty},
+		{TileType::O,TileType::Empty,TileType::Empty}
+	};
+
+	CHECK(score(GameMode::nRow, board) == false);
 
 }
